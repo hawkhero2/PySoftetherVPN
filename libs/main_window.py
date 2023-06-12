460ca6d91d33cebcd867183e678924c0e3dabbd5
Ivanciu Andrei <47902631+hawkhero2@users.noreply.github.com>
2023-06-13 01:31:52 +0300

    wip, table for connections

--- libs/main_window.py
+++ libs/main_window.py
@@ -7,6 +7,19 @@ from libs.CTkTable import *
 setting_file : dict = json.load(open("libs/settings.json"))
 window_size = setting_file.get("window_size")
 
+
+class TableFrame(customtkinter.CTkFrame):
+    def __init__(self, master, **kwargs):
+        super().__init__(master, **kwargs)
+
+        self.values = [["test","test2","test3"],
+                       ["test","test2","test3"]
+            ]
+
+        self.table = CTkTable(master=self, row = 4 , column = 3, values=self.values )
+        self.table.pack(expand=True, fill="both")
+                
+
 # TODO Implement grids
 # TODO Use subprocess instead of os.system, to catch output of terminal(catch_output=true)
 # TODO Figure out managing outputs from subprocess calls/runs
@@ -17,7 +30,7 @@ class Main(customtkinter.CTk):
         self.title("VPN")
         self.geometry(window_size)
         self.resizable(False,False)
-
+        self.grid_rowconfigure(0, 1, weight = 1)
         self.settings_file = json.load(open("libs/settings.json"))
         self.disconnect_btn = customtkinter.CTkButton(self, 
                                                 text = "Disconnect",  
@@ -33,16 +46,7 @@ class Main(customtkinter.CTk):
                                             command = self.button_connect_vpn)
         self.connect_btn.place(relx = 0.35, rely = 0.8)
         self.settings.place(relx = 0.05, rely = 0.8)
-        # self.connections_view = TableFrame()
-        
-        self.values = [["test","test2","test3"],
-                       ["test","test2","test3"]
-            ]
-
-        self.frame = customtkinter.CTkFrame(self, width = 250, height = 150)
-        self.frame.place(relx = 0.1, rely = 0.1)
-        self.table = CTkTable(self.frame, row = 4 , column = 3, values=self.values )
-        self.table.pack(expand=True, fill="both")
+        self.connections = TableFrame(master = self)
         self.settings_window : customtkinter.CTkToplevel = None
     
     def settings_open(self):
