915a96170e4b54f24d04bac94b7b904179cef80b
Ivanciu Andrei <47902631+hawkhero2@users.noreply.github.com>
2023-06-22 23:59:33 +0300

    nothing shows

--- libs/main_window.py
+++ libs/main_window.py
@@ -28,15 +28,36 @@ class TopFrame(customtkinter.CTkFrame):
 
 
 class LowerFrame(customtkinter.CTkFrame):
+    def settings_call(self):
+        if self.settings_window is None or not self.settings_window.winfo_exists():
+            self.settings_window = Settings()
+            self.settings_window.focus()
+        else:
+            if self.settings_window.state() == "withdrawn":
+                self.settings_window.deiconify()
+        
+   
+    
+    def connect_button_cmd(self):
+        pass
+    
+    def disconnect_con(self):
+        pass   
+    
     def __init__(self, master, **kwargs):
         super().__init__(master, **kwargs)
 
         self.grid_rowconfigure(0, weight=1)
-        self.grid_rowconfigure(0, weight=1)
-        self.button = customtkinter.CTkButton(self, width=140, height=70)
-        self.button.grid(row = 0, column = 0, padx = 10, pady = 10) 
-        
+        self.grid_columnconfigure(3, weight=1)
 
+        self.connect_button =  customtkinter.CTkButton(self, text = "Connect", command = self.connect_button_cmd)
+        self.connect_button.grid(row = 0, column = 1, padx = 10, pady = 10 , sticky = "ew")
+        self.disconect_button = customtkinter.CTkButton(self, text="Disconnect", command=self.disconnect_con)
+        self.disconect_button.grid(row = 0, column = 0, padx = 10, pady = 10) 
+        self.settings_button = customtkinter.CTkButton(self, text = "Settings", command = self.settings_call)
+        self.settings_button.grid(row = 0, column = 2, padx = 10, pady = 10, sticky = "ew" )
+        
+ 
 # class TableFrame(customtkinter.CTkFrame):
 #     def __init__(self, master, **kwargs):
 #         super().__init__(master, **kwargs)
@@ -60,49 +81,26 @@ class Main(customtkinter.CTk):
         self.geometry(window_size)
         self.resizable(False,False)
         self.grid_rowconfigure(2, weight = 1)
-        self.upper_frame = customtkinter.CTkFrame(self)
-        self.lower_frame = customtkinter.CTkFrame(self)
+        self.upper_frame = TopFrame(self)
+        self.lower_frame = LowerFrame(self)
         
-        self.settings_file = json.load(open("libs/settings.json"))
-        self.disconnect_btn = customtkinter.CTkButton(self, 
-                                                text = "Disconnect",  
-                                                command =self.button_disconnect_vpn)
+        # self.settings_file = json.load(open("libs/settings.json"))
+        # self.disconnect_btn = customtkinter.CTkButton(self, 
+        #                                         text = "Disconnect",  
+        #                                         command =self.button_disconnect_vpn)
 
-        # self.disconnect_btn.place(relx = 0.65, rely = 0.8)
-        self.disconnect_btn.grid( row=2 , padx = 20, pady = 20)
-        self.connection_list =self.get_connections()
-        self.option_menu = customtkinter.CTkOptionMenu(self, width = 150, height = 34, 
-                                                       values = self.connection_list, 
-                                                       command = self. select_connection)
-        self.settings = customtkinter.CTkButton(self, 
-                                            text = "Settings", 
-                                            command = self.settings_open)
-        self.connect_btn = customtkinter.CTkButton(self, 
-                                            text = "Connect",
-                                            command = self.button_connect_vpn)
+       # self.disconnect_btn.place(relx = 0.65, rely = 0.8)
+
+        # self.disconnect_btn.grid( row=2 , padx = 20, pady = 20)
+        # self.connection_list =self.get_connections()
+        # self.option_menu = customtkinter.CTkOptionMenu(self, width = 150, height = 34, 
+        #                                                values = self.connection_list, 
+        #                                                command = self. select_connection)
+        # self.settings = customtkinter.CTkButton(self, 
+        #                                     text = "Settings", 
+        #                                     command = self.settings_open)
+        # self.connect_btn = customtkinter.CTkButton(self, 
+        #                                     text = "Connect",
+        #                                     command = self.button_connect_vpn)
         self.settings_window : customtkinter.CTkToplevel = None
     
-    def settings_open(self):
-        if self.settings_window is None or not self.settings_window.winfo_exists():
-            self.settings_window = Settings()
-            self.settings_window.focus()
-        else:
-            if self.settings_window.state() == "withdrawn":
-                self.settings_window.deiconify()
-        
-
-    def button_disconnect_vpn(self):
-        # os.system("vpnclient stop >/dev/null 2>&1")
-        print("terminating connection...")
-
-    # def get_connections(self):
-    #     connections = setting_file.get("connections")        
-    #     return connections
-        
-    # def select_connection(self, choice):
-    #     pass
-
-    def button_connect_vpn(self):
-        # WIP
-        subprocess.run("vpnclient start", capture_output = True)
-        print("connecting vpnclient...")
