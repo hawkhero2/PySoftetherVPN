3e2abd38f5e04a932e87daf532807f23a9ad8f4f
Ivanciu Andrei <47902631+hawkhero2@users.noreply.github.com>
2023-06-25 20:33:54 +0300

    configured launch file, UI fixed, needs adjust

--- libs/main_window.py
+++ libs/main_window.py
@@ -11,16 +11,18 @@ window_size = setting_file.get("window_size")
 class TopFrame(customtkinter.CTkFrame):
     def __init__(self, master, **kwargs):
         super().__init__(master, **kwargs)
-
+       
+        self.grid_columnconfigure(0, weight=1)
+        
         self.connection_list =self.get_connections()
         self.option_menu = customtkinter.CTkOptionMenu(self, width = 150, height = 34, 
                                                     values = self.connection_list, 
                                                     command = self. select_connection)
 
-        self.option_menu.grid(row = 0, column = 0, padx = 10, pady = 10)
+        self.option_menu.grid(column = 0, sticky = "ew")
 
     def get_connections(self):
-        connections = setting_file.get("connections")        
+        connections = setting_file.get("connections") 
         return connections
         
     def select_connection(self, choice):
@@ -36,7 +38,6 @@ class LowerFrame(customtkinter.CTkFrame):
             if self.settings_window.state() == "withdrawn":
                 self.settings_window.deiconify()
         
-   
     
     def connect_button_cmd(self):
         pass
@@ -52,8 +53,10 @@ class LowerFrame(customtkinter.CTkFrame):
 
         self.connect_button =  customtkinter.CTkButton(self, text = "Connect", command = self.connect_button_cmd)
         self.connect_button.grid(row = 0, column = 1, padx = 10, pady = 10 , sticky = "ew")
+
         self.disconect_button = customtkinter.CTkButton(self, text="Disconnect", command=self.disconnect_con)
-        self.disconect_button.grid(row = 0, column = 0, padx = 10, pady = 10) 
+        self.disconect_button.grid(row = 0, column = 0, padx = 10, pady = 10)
+        
         self.settings_button = customtkinter.CTkButton(self, text = "Settings", command = self.settings_call)
         self.settings_button.grid(row = 0, column = 2, padx = 10, pady = 10, sticky = "ew" )
         
@@ -83,7 +86,8 @@ class Main(customtkinter.CTk):
         self.grid_rowconfigure(2, weight = 1)
         self.upper_frame = TopFrame(self)
         self.lower_frame = LowerFrame(self)
-        
+        self.upper_frame.grid(row = 0)
+        self.lower_frame.grid(row = 1) 
         # self.settings_file = json.load(open("libs/settings.json"))
         # self.disconnect_btn = customtkinter.CTkButton(self, 
         #                                         text = "Disconnect",  
