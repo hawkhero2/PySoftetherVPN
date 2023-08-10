96c2ad917c5a0c39034b28a4981bd8f86f1818da
hawkhero2 <cheseecheseechesee@gmail.com>
2023-08-10 15:27:35 +0300

    working on delete script

--- delete_adapter.py
+++ delete_adapter.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python3
 
 import subprocess
-import os
+
+vpn_name = input("Type name of the VPN Adapter to be deleted")
+
+output = subprocess.run(f"ip a | grep vpn_{vpn_name}", shell=True, capture_output=True)
+print(output.returncode)
+
+if output.returncode == 0:
+    subprocess.run(f"echo There is no vpn adapter with that name")
+else:
+    output = subprocess.run(f"sudo ip link delete vpn_{vpn_name}", shell=True, capture_output=True)
+    print(output.stdout.decode())
+print(output.stdout.decode())
